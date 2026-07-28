6c0443848a1ce8fed91ef37cec2bc1475344a3fe
krunalyelgire <yelgirekrunal@gmail.com>
2026-07-29 00:12:18 +0530

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,1 +1,13 @@
+const empId = localStorage.getItem("empId");
+const empName = localStorage.getItem("empName");
 
+document.getElementById("empId").innerText = empId;
+document.getElementById("empName").innerText = empName;
+
+function logout() {
+
+    localStorage.clear();
+
+    window.location.href = "index.html";
+
+}
