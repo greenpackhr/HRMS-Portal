15b749e7cea01a7a7e5c323b0ef9f0ae7823f8d4
krunalyelgire <yelgirekrunal@gmail.com>
2026-08-06 00:46:19 +0530

    Update leave.js

--- leave.js
+++ leave.js
@@ -42,7 +42,25 @@ fetch(url)
         });
 
 }
+function formatDate(dateValue){
 
+    if(!dateValue) return "";
+
+    let date = new Date(dateValue);
+
+    let day = String(date.getDate()).padStart(2, "0");
+    let month = String(date.getMonth() + 1).padStart(2, "0");
+    let year = date.getFullYear();
+
+    return day + "/" + month + "/" + year;
+
+}
+
+
+function loadLeaveHistory(){
+
+    const empId = localStorage.getItem("empId");
+    
 function loadLeaveHistory(){
 
     const empId = localStorage.getItem("empId");
