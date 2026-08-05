3a0e65132b96671e4138afd68f0f9097220001af
krunalyelgire <yelgirekrunal@gmail.com>
2026-08-06 00:57:33 +0530

    Update leave.js

--- leave.js
+++ leave.js
@@ -12,6 +12,18 @@ function calculateDays(fromDate, toDate){
     return days;
 
 }
+function calculateDays(fromDate, toDate){
+
+    let start = new Date(fromDate);
+    let end = new Date(toDate);
+
+    let difference = end - start;
+
+    let days = Math.floor(difference / (1000 * 60 * 60 * 24)) + 1;
+
+    return days;
+
+}
 function applyLeave() {
 
     const empId = localStorage.getItem("empId");
